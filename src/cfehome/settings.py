610e764b92771b4ae8fb82c583ead9a4539eb9fe
Gaurav Singh <gaurav.singh@crosslynxus.com>
2025-08-20 17:12:47 +0530

    serving static files

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'commando',
     # ✅ your apps
     'visits',
 ]
@@ -114,6 +115,7 @@ USE_TZ = True
 # ✅ Static files
 STATIC_URL = 'static/'
 STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
 STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
 
 
